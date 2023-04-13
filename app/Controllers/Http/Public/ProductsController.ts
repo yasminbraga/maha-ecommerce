70609466cf254941c52e90ca69e3892e59985360
yasminbraga <yasminbragateixeira@gmail.com>
2023-04-13 15:42:58 -0300

    list products and kits

--- app/Controllers/Http/Public/ProductsController.ts
+++ app/Controllers/Http/Public/ProductsController.ts
@@ -1,11 +1,16 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Kit from 'App/Models/Kit'
 import Product from 'App/Models/Product'
 
 export default class ProductsController {
-  public async index({ session, response }: HttpContextContract) {
+  public async index({ session, response, view }: HttpContextContract) {
     try {
       const products = await Product.query().preload('file')
-      return products.map((i) => i.toJSON())
+      const kits = await Kit.query().preload('file').preload('products')
+      return view.render('public/products', {
+        products: products.map((i) => i.toJSON()),
+        kits: kits.map((i) => i.toJSON()),
+      })
     } catch (error) {
       console.error(error)
       session.flash('error', 'Erro ao encontrar produtos')
