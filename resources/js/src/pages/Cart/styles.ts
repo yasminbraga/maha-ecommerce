6f902f104f5628a8992d1f50c41596893f51a06d
yasminbraga <yasminbragateixeira@gmail.com>
2023-06-02 16:03:24 -0300

    add first style cart

--- resources/js/src/pages/Cart/styles.ts
+++ resources/js/src/pages/Cart/styles.ts
@@ -5,11 +5,17 @@ export const CartContent = styled.div`
   display: flex;
   gap: 1rem;
 `
-
 export const Summary = styled.div`
   background-color: #f9f7f5;
   padding: 1rem;
-  /* width: 400px; */
+`
+
+export const InfoContainer = styled.p`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  margin: 0.5rem 0;
+  color: #0d2601;
 `
 export const ProductContainer = styled.div`
   display: flex;
@@ -32,12 +38,19 @@ export const TotalContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin: 0.5rem 0;
 `
 export const PrimaryButton = styled.button`
-  background-color: #0d0d0d;
+  background-color: #0d2601;
   color: #fff;
   border: none;
   width: 100%;
   padding: 0.5rem 0;
   font-weight: 500;
+  cursor: pointer;
+  transition: all 0.3s;
+
+  &:hover {
+    background-color: #90a67b;
+  }
 `
--- resources/js/src/services/repositoryApi.ts
+++ resources/js/src/services/repositoryApi.ts
@@ -2,6 +2,10 @@ import axios from 'axios'
 
 const repositoryApi = axios.create({
   baseURL: 'https://repositoriodefitoingredientes.herokuapp.com',
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+  },
 })
 
 export default repositoryApi
