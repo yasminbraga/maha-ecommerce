e56553d547063190b831dd75118c5cd38602db43
yasminbraga <yasminbragateixeira@gmail.com>
2023-06-06 11:18:50 -0300

    wip fix cors

--- resources/js/src/services/repositoryApi.ts
+++ resources/js/src/services/repositoryApi.ts
@@ -4,6 +4,7 @@ const repositoryApi = axios.create({
   baseURL: 'https://repositoriodefitoingredientes.herokuapp.com',
   headers: {
     'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
     'Content-Type': 'application/json',
   },
 })
