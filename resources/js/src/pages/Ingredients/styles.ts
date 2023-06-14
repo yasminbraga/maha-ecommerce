26ed243fadce0b5c5b14add748ab5747208b69ef
yasminbraga <yasminbragateixeira@gmail.com>
2023-06-14 15:23:57 -0300

    load images

---
+++ resources/js/src/pages/Ingredients/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.div``
+
+export const InfoContainer = styled.div`
+  display: flex;
+`
+
+export const ItemImage = styled.img`
+  width: 100px;
+  height: 100px;
+  object-fit: contain;
+`
