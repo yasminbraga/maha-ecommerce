bd889fdf6d7e1bf260653d6a05c00f52bc22ab93
yasminbraga <yasminbragateixeira@gmail.com>
2023-06-19 08:51:55 -0300

    add result page in react

---
+++ resources/js/src/pages/QuizResult/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// import { Container } from './styles';
+
+const QuizResult: React.FC = () => {
+  return (
+    <div>
+      <h1>Resultado do quiz</h1>
+    </div>
+  )
+}
+
+export default QuizResult
